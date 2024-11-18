98587006dfe435d39f6951194b192a827e12def7
Gibson Cruz <gibson8fla@gmail.com>
2024-11-18 17:50:12 -0300

    feat: adds props in components

--- budget-manager/src/App.jsx
+++ budget-manager/src/App.jsx
@@ -1,17 +1,36 @@
+/* eslint-disable no-unused-vars */
+import { data } from "autoprefixer";
 import BugdgetTable from "./Components/BudgetTable/BudgetTable";
 import Filter from "./Components/Filter/Filter";
 import Graph from './Components/Graph/Graph'
 import "./index.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [budgetData, setBudgetData] = useState([]);
+
+  useEffect(() => {
+    const getBudget = async () => {
+      const response = await fetch('https://sheet2api.com/v1/VFJVTlUeRMe2/budget', {
+        method: 'GET',
+      })
+      const data = await response.json();
+      setBudgetData(data);
+
+    }
+    getBudget();
+  }, [data])
+
+
+
   return (
     <section className="flex justify-around p-12 flex-1">
       <section className="w-1/2">
-        <Graph />
+        <Graph dBudget={budgetData} />
       </section>
       <section>
         <Filter />
-        <BugdgetTable />
+        <BugdgetTable data={budgetData} />
       </section>
     </section>
   );
