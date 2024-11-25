a8334b0e15249cedde9ca9df9103699040b42ab6
Gibson Cruz <gibson8fla@gmail.com>
2024-11-24 22:12:34 -0300

    feature: adds fitlter per date

--- budget-manager/src/Components/Filter/Filter.jsx
+++ budget-manager/src/Components/Filter/Filter.jsx
@@ -10,14 +10,14 @@ const Filter = ({ setFilter }) => {
     }
 
     return (
-        <div className="flex-col text-center p-4">
+        <div className="flex flex-col text-center p-4 items-center">
             <span className="text-2xl">Enter date</span>
-            <div className="flex gap-3 p-4 justify-center">
+            <div className="sm:flex sm:flex-row flex-col  gap-3 p-2 w-min justify-center items-center">
                 <label htmlFor="start">Start</label>
                 <input onChange={(e) => setStart(e.target.value)} className="rounded-md text-sky-950" type="date" />
                 <label htmlFor="end">End</label>
                 <input onChange={(e) => setEnd(e.target.value)} className="rounded-md text-sky-950" type="date" />
-                <button className="flex w-max text-center justify-center border-gray-700 hover:shadow-md hover:shadow-slate-100 hover:scale-105 border bg-gradient-to-bl from-gray-900 to-gray-500  p-1 rounded-md" onClick={() => aplyFilter()}>Find</button>
+                <button className="flex w-max text-center justify-center border-gray-700 hover:shadow-md hover:shadow-slate-100 hover:scale-105 border bg-gradient-to-bl from-gray-900 to-gray-500  p-1 rounded-md sm:m-auto ml-12 mt-3" onClick={() => aplyFilter()}>Find</button>
             </div>
         </div>
     )
